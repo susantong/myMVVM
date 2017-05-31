9f6b180841dfcf74907e6324778af0605f2ad42b
susantong <1316160497@qq.com>
2017-05-31 21:23:36 +0800

    实现插值

--- src/vue.js
+++ src/vue.js
@@ -1,7 +1,7 @@
 function vue(oop) {
 	var node = document.querySelector(oop.el);
 	var data = oop.data;
-	var vueArr = ['s-text', 's-show', 's-if', 's-model', 's-mustache'];//指令存储数组
+	var vueArr = ['s-text', 's-show', 's-if', 's-model', 's-for'];//指令存储数组
 
 	this.cache = [];
 
@@ -17,36 +17,115 @@ function vue(oop) {
 		}
 	};
 
+
 	this.compile = function(dom) {
 		this.addQuene(dom);
 	};
 
+	this.muscha = function(value) {
+		var temp = {
+			string: [],
+			text: [],
+			tag: 0
+		};
+
+		var reg = /\{\{((?:.|\\n)+?)\}\}/g;
+
+		if (!reg.test(value)) {
+		    	return null;
+		}
+
+		var match;
+		
+		var lastIndex = reg.lastIndex = 0;
+		var index;
+
+		while(match = reg.exec(value)) {
+			
+			index = match.index;
+
+			if (index > lastIndex) {
+				temp.tag = 1;
+				temp.string.push(value.slice(lastIndex, index));
+			}
+		
+ 			temp.text.push(match[1]);
+			lastIndex = index + match[0].length;
+		}
+		
+		if (lastIndex < value.length) {
+			temp.string.push(value.slice(lastIndex));
+		}
+		return temp;
+	}
+
 	this.pasers = function() {
-		//console.log(this.cache[1]);
 		var that = this;
+		
 		this.cache = this.cache.map(function(node) {
 			var temp = {node: node};
-			//console.log(node.getAttribute('s-if'));
+			if(node.childNodes.length === 1 && node.childNodes[0].nodeType === 3) {
+				temp.muscha = that.muscha(node.innerHTML);	
+			}
+
+			if (node.parentNode !== that.cache[0]) {
+				temp.parentNode = node.parentNode;
+			}
+			
 			vueArr.forEach(function(attr) {
 				if (node.getAttribute(attr)) {
 					temp[attr] = node.getAttribute(attr);
+					if (attr === 's-model') {
+						node.addEventListener('input', change);	//监听input值，实现data的双向改变
+					}
 				}
-				if (node.getAttribute('s-model')) {
-					node.addEventListener('input', change);	//监听input值，实现data的双向改变
-				}
-
+				
 				function change() {
 					data[node.getAttribute(attr)] = event.target.value;
 				};
 			});
 			return temp;
 		});
-
-
 	};
 
-	this.toChange = function(node, attr, content) {
-		//console.log(node, attr, content);
+	this.strSplit = function(item) {
+		var str = '';
+
+		if (!item.muscha.tag) {
+			var len = item.muscha.string.length;
+			switch(len) {
+				case 0:
+					str = data[item.muscha.text[0]];
+					break;
+				case 1:
+					str = data[item.muscha.text[0]] + item.muscha.string[0];
+					break;
+				default:
+					for (var i = 0; i < len; i++) {
+						str = data[item.muscha.text[i]] + item.muscha.string[i];
+					}
+					str += data[item.muscha.text[i]];
+			}
+		} else {
+			var len = item.muscha.text.length;
+			switch(len) {
+				case 1:
+					//console.log(data[item.muscha.text[0]]);
+					str = item.muscha.string[0] + data[item.muscha.text[0]];
+					break;
+				default:
+					for (var j = 0; j < len; j++) {
+						str = item.muscha.string[j] + data[item.muscha.text[j]];
+					}
+					str += item.muscha.string[j];
+			}
+		}
+		return str;
+		//item.node.innerHTML = str;	
+	}
+
+	this.toChange = function(item, attr, content) {
+		var node = item.node;
 		switch(attr) {
 
 			case 's-text':
@@ -75,18 +154,39 @@ function vue(oop) {
 				node.value = content;
 				break;
 
+			case 's-for':
+				console.log(content);
+				break;
+
 		};
 	};
 
 	this.render = function() {
 		//分别实现
 		var that = this;
+
 		this.cache.forEach(function(item) {
+			
 			vueArr.forEach(function(attr) {
 				if (item[attr]) {
-					that.toChange(item.node, attr, data[item[attr]]);
+					var content = data[item[attr]];
+					if (attr === 's-for') {
+						var value = item[attr].replace(/(^\s*)|(\s*$)/g, '');
+						var arr = value.split(' ');
+						var list = that.muscha(item.node.innerHTML);
+						if (arr.length > 1) {
+							item.list = arr[0];
+							//console.log((/^\..*$/.exec(list.text[0]))[1]);
+						}
+						//console.log(list);
+					}
+					that.toChange(item, attr, content);
 				}
 			});
+
+			if (item.muscha) {
+				item.node.innerHTML = that.strSplit(item);
+			}
 		});
 	};
 
@@ -106,13 +206,14 @@ function vue(oop) {
 						//console.log(newVal);
 						val = newVal;
 						//通知所有订阅者改变
+						
 						that.cache.forEach(function(item) {
 							if (item[prop]) {
 								item[prop] = newVal;
 							}
 						});
 						that.callback();
-					},
+					},    
 					enumerable: true,
     				configurable: true
 				});
@@ -130,7 +231,7 @@ function vue(oop) {
 		this.compile(node);
 		//console.log(this.cache);
 		this.pasers();
-		//console.log(this.cache);
+		console.log(this.cache);
 		this.observe();
 		this.render();
 	};
